c37928aa973a2016332c588bb08612c7609bd40a
Mindey <mindey@qq.com>
2016-09-30 01:37:16 +0000

    made a few changes to accomodate multilingual DataFrame

--- asterisk/wikidata.py
+++ asterisk/wikidata.py
@@ -14,6 +14,10 @@ def get_json(wikidata_id):
         {'success': False}
 
 def get_lang(d):
+    ''' 
+    >>> get_lang({'en': 'love', 'zh': u'爱'}) 
+    love 
+    '''
     priorities = ['en', 'zh', 'de', 'ru', 'fr']
     for lang in priorities:
         if d.get(lang):
@@ -22,10 +26,35 @@ def get_lang(d):
 
 def Concept(wikidata_id):
 
+    '''
+    INPUT
+
+    The wikidata ID (str), or l (dict).
+
+    Custom being useful when there is no internet, and we want, e.g., represent a concept in multiple languages.
+    Example of custom:
+
+    l = {'en': 'love', 'zh': u'爱'}
+
+    OUTPUT
+
+    Python class, with the name corresponding to WikiData ID. Otherwise, 'Q0'.
+
+    '''
+
     # Defining Classes Programmatically:
     # http://chimera.labs.oreilly.com/books/1230000000393/ch09.html#_defining_classes_programmatically
 
-    _concept = get_json(wikidata_id)
+
+    if isinstance(wikidata_id, str):
+        _concept = get_json(wikidata_id)
+    elif isinstance(wikidata_id, dict):
+        _concept = {'entities': {'Q0': {'aliases': {v:[{'value':wikidata_id[v]}] for k,v in enumerate(wikidata_id)}, 'claims': {} }} }
+        wikidata_id = 'Q0'
+    else:
+        return None 
+
+
     _alias = get_lang(_concept['entities'][wikidata_id]['aliases'])[0]['value']
 
     class Name(): pass
@@ -35,9 +64,9 @@ def Concept(wikidata_id):
 
     def _repr(self):
         if self.details:
-            return '%s (%s)' % (wikidata_id, self.details)
+            return ('%s (%s)' % (_alias, self.details)).encode('utf8')
         else:
-            return str(wikidata_id)
+            return _alias.encode('utf8')
 
     def _lang(self, *args):
         if len(args) > 1:
@@ -47,12 +76,13 @@ def Concept(wikidata_id):
                 s[lang] = val
             return s
         elif len(args) == 1:
-            return self.__class__.concept['entities'][wikidata_id]['aliases'].get(args[0])[0]['value']
+            __alias = self.__class__.concept['entities'][wikidata_id]['aliases'].get(args[0])[0]['value']
+            return ('%s (%s)' % (__alias, self.details)).encode('utf8')
         else:
             return get_lang(self.__class__.concept['entities'][wikidata_id]['aliases'])[0]['value']
 
     def _unicode(self):
-        return '%s (%s)' % (self.u(), self.details)
+        return '%s' % (_alias,)
 
     propositions = ''
 
@@ -77,4 +107,8 @@ def Concept(wikidata_id):
     return Name
 
 def Instance(wikidata_id):
-    return Concept(wikidata_id)()
+    concept = Concept(wikidata_id)
+    if concept:
+        return concept()
+    else:
+        return None
